e3a0a03fef746f74642f0dd440f64fcb102328a9
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-11-25 23:55:53 +0300

    .

---
+++ app/search/Search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Search() {
+    return <div>Seacrh component</div>;
+}
+
+export default Search;
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-    return <div>TODOS</div>;
+    return (
+        <div>
+            <h1>Todos list...</h1>
+        </div>
+    );
 };
 
 export default page;
