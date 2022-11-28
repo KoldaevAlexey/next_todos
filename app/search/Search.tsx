bce58ff7263144e672355eb5187f0123a13147c1
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-11-28 23:54:14 +0300

    .

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 function Search() {
-    return <div>Seacrh component</div>;
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+
+    const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setSearch("");
+        router.push(`/search/${search}`);
+    };
+
+    return (
+        <form onSubmit={handleSearch}>
+            <input
+                type="text"
+                value={search}
+                placeholder="Enter the Search term"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg"
+            >
+                Search
+            </button>
+        </form>
+    );
 }
 
 export default Search;
